9c37b9a7be846f936cbd9cc957802ea4efca4d56
kavindihewawasam <kwathsalah@gmail.com>
2024-12-07 15:06:17 +0530

    Updated MainContent.js with Grid layout and integrated StatusBar component.

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import "./MainContent.css";
+import { Grid } from "@material-ui/core";
+// import MainPage from "./Components/MainPage/MainPage";
+// import StatusBar from "./Components/StatusBar/StatusBar";
+import StatusBar from "../StatusBar/StatusBar";
 
 export class MainContent extends Component {
   constructor(props) {
@@ -8,7 +12,25 @@ export class MainContent extends Component {
     this.state = {};
   }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Grid container>
+          <Grid item={2}></Grid>
+          <Grid item={6}>
+            <div>
+              <StatusBar />
+              {/* <MainPage /> */}
+            </div>
+          </Grid>
+          <Grid>
+            <Grid item={2}></Grid>
+          </Grid>
+          <Grid>
+            <Grid item={2}></Grid>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
